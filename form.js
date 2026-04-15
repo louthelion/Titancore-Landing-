9cd636499ee16549123b230522189038c6d0c00f
louthelion <holdingstitancore@gmail.com>
2026-04-15 03:49:53 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
-  if (!form) return;
 
-  const thanksBox = document.querySelector(".thanks");
-  if (thanksBox) {
-    thanksBox.style.display = "none";
-  }
-
-  form.addEventListener("submit", function () {
-    if (thanksBox) {
-      setTimeout(() => {
-        thanksBox.style.display = "block";
-        thanksBox.scrollIntoView({ behavior: "smooth", block: "start" });
-      }, 600);
-    }
-  });
-});
